27381f57c10ab039f07e6ecb834b8be84420f537
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2024-08-18 19:19:46 -0300

    .

--- ta10.js
+++ ta10.js
@@ -1,4 +1,16 @@
-function getSum(nums) {
-    const total = nums.reduce((a, b) => a + b);
-    console.log(total);
+function duplicates(nums) {
+    const counts = {};
+    let duplicatesCount = 0;
+
+    nums.forEach(num => {
+        counts[num] = (counts[num] || 0) + 1;
+    });
+
+    for (let num in counts) {
+        if (counts[num] > 1) {
+            duplicatesCount++;
+        }
+    }
+
+    console.log(duplicatesCount);
 }
